5dbee589c128b9371698c6f1019831ce45c13e57
Alex Axthelm <aaxthelm@RMI.org>
2024-06-11 17:54:45 +0200

    update run script

--- inst/extdata/scripts/run_pacta.R
+++ inst/extdata/scripts/run_pacta.R
@@ -1,5 +1,4 @@
 logger::log_threshold(Sys.getenv("LOG_LEVEL", "WARN"))
-cat(commandArgs(trailingOnly = TRUE))
 workflow.pacta:::run_pacta(commandArgs(trailingOnly = TRUE))
 workflow.pacta.report:::run_pacta_reporting_process(
   commandArgs(trailingOnly = TRUE)
