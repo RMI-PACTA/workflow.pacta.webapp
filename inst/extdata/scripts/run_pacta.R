9ef309dd248e0412509fc440852271306289ba68
Alex Axthelm <aaxthelm@RMI.org>
2024-06-11 16:22:02 +0200

    debugging: cat params before running

--- inst/extdata/scripts/run_pacta.R
+++ inst/extdata/scripts/run_pacta.R
@@ -1,4 +1,5 @@
 logger::log_threshold(Sys.getenv("LOG_LEVEL", "WARN"))
+cat(commandArgs(trailingOnly = TRUE))
 workflow.pacta:::run_pacta(commandArgs(trailingOnly = TRUE))
 workflow.pacta.report:::run_pacta_reporting_process(
   commandArgs(trailingOnly = TRUE)
