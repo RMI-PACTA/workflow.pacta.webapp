79053ab0ed255995977e8df6a5086d48f73dd52b
Alex Axthelm <aaxthelm@RMI.org>
2024-06-11 12:49:57 +0200

    Linting (line length)

--- inst/extdata/scripts/run_pacta.R
+++ inst/extdata/scripts/run_pacta.R
@@ -1,3 +1,5 @@
 logger::log_threshold(Sys.getenv("LOG_LEVEL", "WARN"))
 workflow.pacta:::run_pacta(commandArgs(trailingOnly = TRUE))
-workflow.pacta.report:::run_pacta_reporting_process(commandArgs(trailingOnly = TRUE))
+workflow.pacta.report:::run_pacta_reporting_process(
+  commandArgs(trailingOnly = TRUE)
+)
