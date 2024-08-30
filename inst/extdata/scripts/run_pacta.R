7092eb65b8fe2acadf635f5495044ca00dca082b
Alex Axthelm <aaxthelm@RMI.org>
2024-08-30 13:48:55 +0200

    Change calling script to adapt to workflow.pacta

--- inst/extdata/scripts/run_pacta.R
+++ inst/extdata/scripts/run_pacta.R
@@ -1,5 +1,24 @@
 logger::log_threshold(Sys.getenv("LOG_LEVEL", "INFO"))
-workflow.pacta:::run_pacta(commandArgs(trailingOnly = TRUE))
+
+raw_params <- commandArgs(trailingOnly = TRUE)
+params <- pacta.workflow.utils::parse_raw_params(
+  json = raw_params,
+  inheritence_search_paths = system.file(
+    "extdata", "parameters",
+    package = "workflow.pacta"
+  ),
+  schema_file = system.file(
+    "extdata", "schema", "portfolioParameters.json",
+    package = "workflow.pacta"
+  ),
+  raw_schema_file = system.file(
+    "extdata", "schema", "rawParameters.json",
+    package = "workflow.pacta"
+  )
+)
+
+manifest_info <- workflow.pacta::run_pacta(params)
+
 workflow.pacta.report:::run_pacta_reporting_process(
   commandArgs(trailingOnly = TRUE)
 )
