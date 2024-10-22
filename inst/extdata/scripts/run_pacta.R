05d29a43687730be13d93b2600c54fddb989be61
Alex Axthelm <aaxthelm@RMI.org>
2024-10-22 19:13:36 +0200

    Export report manifest

--- inst/extdata/scripts/run_pacta.R
+++ inst/extdata/scripts/run_pacta.R
@@ -27,6 +27,14 @@ pacta.workflow.utils::export_manifest(
   raw_params = raw_params
 )
 
-workflow.pacta.report:::run_pacta_reporting_process(
-  commandArgs(trailingOnly = TRUE)
+report_manifest_info <- workflow.pacta.report::run_pacta_reporting_process(
+  params = params
+)
+
+pacta.workflow.utils::export_manifest(
+  input_files = report_manifest_info[["input_files"]],
+  output_files = report_manifest_info[["output_files"]],
+  params = report_manifest_info[["params"]],
+  manifest_path = file.path(Sys.getenv("REPORT_OUTPUT_DIR"), "manifest.json"),
+  raw_params = raw_params
 )
