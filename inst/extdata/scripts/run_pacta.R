f01cd9397bdcc6c7f982fbe3896408357df08c94
Alex Axthelm <aaxthelm@RMI.org>
2024-11-08 11:15:00 +0100

    WIP - inheritence

--- inst/extdata/scripts/run_pacta.R
+++ inst/extdata/scripts/run_pacta.R
@@ -1,19 +1,76 @@
 logger::log_threshold(Sys.getenv("LOG_LEVEL", "INFO"))
 
+# Copy Schema files
+invisible(
+  file.copy(
+    from = system.file(
+      "extdata", "schema", "portfolio.json",
+      package = "workflow.pacta"
+      ),
+    to = file.path(
+      system.file(
+        "extdata", "schema",
+        package = "workflow.pacta.webapp"
+        ),
+      "portfolio.json"
+    )
+  )
+)
+invisible(
+  file.copy(
+    from = system.file(
+      "extdata", "schema", "portfolioParameters.json",
+      package = "workflow.pacta"
+      ),
+    to = file.path(
+      system.file(
+        "extdata", "schema",
+        package = "workflow.pacta.webapp"
+        ),
+      "portfolioParameters.json"
+    )
+  )
+)
+invisible(
+  file.copy(
+    from = system.file(
+      "extdata", "schema", "reportingParameters.json",
+      package = "workflow.pacta.report"
+      ),
+    to = file.path(
+      system.file(
+        "extdata", "schema",
+        package = "workflow.pacta.webapp"
+        ),
+      "reportingParameters.json"
+    )
+  )
+)
+
 raw_params <- commandArgs(trailingOnly = TRUE)
 params <- pacta.workflow.utils::parse_raw_params(
   json = raw_params,
-  inheritence_search_paths = system.file(
-    "extdata", "parameters",
-    package = "workflow.pacta"
+  inheritence_search_paths = c(
+    system.file(
+      "extdata", "parameters",
+      package = "workflow.pacta.webapp"
+    ),
+    system.file(
+      "extdata", "parameters",
+      package = "workflow.pacta"
+    ),
+    system.file(
+      "extdata", "parameters",
+      package = "workflow.pacta.report"
+    )
   ),
   schema_file = system.file(
-    "extdata", "schema", "portfolioParameters.json",
-    package = "workflow.pacta"
+    "extdata", "schema", "PACTAParameters.json",
+    package = "workflow.pacta.webapp"
   ),
   raw_schema_file = system.file(
     "extdata", "schema", "rawParameters.json",
-    package = "workflow.pacta"
+    package = "workflow.pacta.webapp"
   )
 )
 
