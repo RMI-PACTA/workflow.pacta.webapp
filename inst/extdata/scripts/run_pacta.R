2921979701ffdcfdd5d507e3d237649a05e5f066
Alex Axthelm <aaxthelm@RMI.org>
2024-11-12 17:28:34 +0100

    update calling script

--- inst/extdata/scripts/run_pacta.R
+++ inst/extdata/scripts/run_pacta.R
@@ -32,24 +32,9 @@ params <- pacta.workflow.utils::parse_raw_params(
   )
 )
 
-manifest_info <- workflow.pacta::run_pacta(params)
-
-pacta.workflow.utils::export_manifest(
-  input_files = manifest_info[["input_files"]],
-  output_files = manifest_info[["output_files"]],
-  params = manifest_info[["params"]],
-  manifest_path = file.path(Sys.getenv("ANALYSIS_OUTPUT_DIR"), "manifest.json"),
-  raw_params = raw_params
-)
-
-report_manifest_info <- workflow.pacta.report:::run_pacta_reporting_process(
-  params = params
-)
-
-pacta.workflow.utils::export_manifest(
-  input_files = report_manifest_info[["input_files"]],
-  output_files = report_manifest_info[["output_files"]],
-  params = report_manifest_info[["params"]],
-  manifest_path = file.path(Sys.getenv("REPORT_OUTPUT_DIR"), "manifest.json"),
-  raw_params = raw_params
+workflow.pacta.webapp::run_webapp_workflow(
+  params = params,
+  raw_params = raw_params,
+  run_analysis = TRUE,
+  run_reporting = TRUE
 )
