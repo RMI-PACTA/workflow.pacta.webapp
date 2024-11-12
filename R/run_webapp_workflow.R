077d7e176856f57b9a8cd98de88ca6be7a2335ca
Alex Axthelm <aaxthelm@RMI.org>
2024-11-12 17:27:16 +0100

    Update conditionals for reporting and analysis

--- R/run_webapp_workflow.R
+++ R/run_webapp_workflow.R
@@ -1,29 +1,47 @@
 run_webapp_workflow <- function(
   params,
+  raw_params,
   run_analysis = TRUE,
   run_reporting = TRUE
   ) {
 
-  analysis_manifest_info <- workflow.pacta::run_pacta(params)
-
-  pacta.workflow.utils::export_manifest(
-    input_files = manifest_info[["input_files"]],
-    output_files = manifest_info[["output_files"]],
-    params = manifest_info[["params"]],
-    manifest_path = file.path(Sys.getenv("ANALYSIS_OUTPUT_DIR"), "manifest.json"),
-    raw_params = raw_params
+  analysis_manifest_path <- file.path(
+    Sys.getenv("ANALYSIS_OUTPUT_DIR"),
+    "manifest.json"
+  )
+  reporting_manifest_path <- file.path(
+    Sys.getenv("REPORTING_OUTPUT_DIR"),
+    "manifest.json"
   )
 
-  report_manifest_info <- workflow.pacta.report:::run_pacta_reporting_process(
-    params = params
-  )
+  if (run_analysis || !file.exists(analysis_manifest_path)) {
 
-  pacta.workflow.utils::export_manifest(
-    input_files = report_manifest_info[["input_files"]],
-    output_files = report_manifest_info[["output_files"]],
-    params = report_manifest_info[["params"]],
-    manifest_path = file.path(Sys.getenv("REPORT_OUTPUT_DIR"), "manifest.json"),
-    raw_params = raw_params
-  )
+    analysis_manifest_info <- workflow.pacta::run_pacta(params)
+
+    pacta.workflow.utils::export_manifest(
+      input_files = analysis_manifest_info[["input_files"]],
+      output_files = analysis_manifest_info[["output_files"]],
+      params = analysis_manifest_info[["params"]],
+      manifest_path = analysis_manifest_path,
+      raw_params = raw_params
+    )
+
+  }
+
+  if (run_reporting) {
+
+    report_manifest_info <- workflow.pacta.report:::run_pacta_reporting_process(
+      params = params
+    )
+
+    pacta.workflow.utils::export_manifest(
+      input_files = report_manifest_info[["input_files"]],
+      output_files = report_manifest_info[["output_files"]],
+      params = report_manifest_info[["params"]],
+      manifest_path = reporting_manifest_path,
+      raw_params = raw_params
+    )
+
+  }
 
 }
