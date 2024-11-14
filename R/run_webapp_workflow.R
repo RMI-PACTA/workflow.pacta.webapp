272d14b076046526797ca90fed61c0e28e859ac6
Alex Axthelm <aaxthelm@RMI.org>
2024-11-14 11:56:38 +0100

    Explicitly pass i/o directories

--- R/run_webapp_workflow.R
+++ R/run_webapp_workflow.R
@@ -13,7 +13,16 @@ run_webapp_workflow <- function(
   params,
   raw_params,
   run_analysis = TRUE,
-  run_reporting = TRUE
+  run_reporting = TRUE,
+  analysis_output_dir = Sys.getenv("ANALYSIS_OUTPUT_DIR"),
+  benchmarks_dir = Sys.getenv("BENCHMARKS_DIR"),
+  pacta_data_dir = Sys.getenv("PACTA_DATA_DIR"),
+  portfolio_dir = Sys.getenv("PORTFOLIO_DIR"),
+  real_estate_dir = Sys.getenv("REAL_ESTATE_DIR"),
+  report_output_dir = Sys.getenv("REPORT_OUTPUT_DIR"),
+  score_card_dir = Sys.getenv("SCORE_CARD_DIR"),
+  summary_output_dir = Sys.getenv("SUMMARY_OUTPUT_DIR"),
+  survey_dir = Sys.getenv("SURVEY_DIR")
 ) {
 
   analysis_manifest_path <- file.path(
@@ -21,13 +30,18 @@ run_webapp_workflow <- function(
     "manifest.json"
   )
   reporting_manifest_path <- file.path(
-    Sys.getenv("REPORTING_OUTPUT_DIR"),
+    Sys.getenv("REPORT_OUTPUT_DIR"),
     "manifest.json"
   )
 
   if (run_analysis || !file.exists(analysis_manifest_path)) {
 
-    analysis_manifest_info <- workflow.pacta::run_pacta(params)
+    analysis_manifest_info <- workflow.pacta::run_pacta(
+      params = params,
+      pacta_data_dir = pacta_data_dir,
+      output_dir = analysis_output_dir,
+      portfolio_dir = portfolio_dir
+    )
 
     pacta.workflow.utils::export_manifest(
       input_files = analysis_manifest_info[["input_files"]],
@@ -42,7 +56,14 @@ run_webapp_workflow <- function(
   if (run_reporting) {
 
     report_manifest_info <- workflow.pacta.report::run_pacta_reporting_process(
-      params = params
+      params = params,
+      analysis_output_dir = analysis_output_dir,
+      benchmarks_dir = benchmarks_dir,
+      report_output_dir = report_output_dir,
+      summary_output_dir = summary_output_dir,
+      real_estate_dir = real_estate_dir,
+      survey_dir = survey_dir,
+      score_card_dir = score_card_dir
     )
 
     pacta.workflow.utils::export_manifest(
