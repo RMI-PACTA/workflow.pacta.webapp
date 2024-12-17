ee48a9930d359e6da3f65430e3b0d33f0556136d
Alex Axthelm <aaxthelm@RMI.org>
2024-12-17 15:31:30 +0100

    Use params to control workflow steps

--- inst/extdata/scripts/run_pacta.R
+++ inst/extdata/scripts/run_pacta.R
@@ -30,7 +30,7 @@ params <- pacta.workflow.utils::parse_raw_params(
 workflow.pacta.webapp::run_webapp_workflow(
   params = params,
   raw_params = raw_params,
-  run_audit = TRUE,
-  run_analysis = TRUE,
-  run_reporting = TRUE
+  run_audit = params[["commands"]][["audit"]],
+  run_analysis = params[["commands"]][["analysis"]]
+  run_reporting = params[["commands"]][["report"]]
 )
